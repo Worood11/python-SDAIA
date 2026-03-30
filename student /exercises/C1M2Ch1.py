886a526325e99226566b2083028755bb2a19a85c
Worood <woroodmobarki@gmail.com>
2026-03-30 22:18:12 +0300

    Ch1E4

--- student /exercises/C1M2Ch1.py
+++ student /exercises/C1M2Ch1.py
@@ -27,3 +27,34 @@ print(abs) # built-in function
 print(int) #class
 
 print(type) #class
+
+#Ex4
+
+#1
+minutes = 42
+seconds = 42
+total_sec = (minutes * 60) + seconds
+print(total_sec)
+
+#2
+kilometers = 10
+km_per_mile = 1.61
+total_km = kilometers / km_per_mile
+print(total_km)
+
+#3
+seconds_per_mile = total_sec / total_km
+print(seconds_per_mile)
+
+#4
+pace_in_seconds = 412.48
+minutes = int(pace_in_seconds // 60)
+seconds = pace_in_seconds % 60
+print(f"{minutes} minutes and {seconds:.2f} seconds")
+
+#5
+total_miles = 6.21118
+total_seconds = 2562
+hours = total_seconds / 3600
+speed = total_miles / hours
+print(speed)
