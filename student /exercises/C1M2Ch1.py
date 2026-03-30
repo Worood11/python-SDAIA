6fe7fcecd4a5df6a2447cb21f250ac9305e3cdc7
Worood <woroodmobarki@gmail.com>
2026-03-30 21:57:24 +0300

    Ch1E2

--- student /exercises/C1M2Ch1.py
+++ student /exercises/C1M2Ch1.py
@@ -3,3 +3,10 @@
 print(round(42.5)) 
 print(round(43.5)) #43.5 => 44.5
 
+#Ex2
+#here useing ++ in python it will act like it's 2+2 and the result is 4
+print(2++2)
+# python will throw an SyntaxError
+#print(4 2)
+#it will throw a SyntaxError
+#print(round(42.5)
